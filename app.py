d7597fee926e412ef7b7f2fc24aabb7822f8194c
khaled03282016 <omrikhaledr@gmail.com>
2020-10-22 09:54:49 -0400

    second Commit clientMongo

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify, url_for, session, escape
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.secret_key = 'eCommerce'
 
-app.config ["MONGO_DBNAME"] = "eCommerce_shop"
-app.config["MONGO_URI"] = "mongodb+srv://khaled:sabrina03282016@cluster0.7kuww.mongodb.net/eCommerce_shop?retryWrites=true&w=majority"
-mongo = PyMongo(app)
-
+# app.config ["MONGO_DBNAME"] = "eCommerce_shop"
+# app.config["MONGO_URI"] = "mongodb+srv://khaled:sabrina03282016@cluster0.7kuww.mongodb.net/eCommerce_shop?retryWrites=true&w=majority"
+client = MongoClient("mongodb+srv://khaled:sabrina03282016@cluster0.7kuww.mongodb.net/eCommerce_shop?retryWrites=true&w=majority")
+mongo = client["eCommerce_shop"]
 @app.route('/get', methods=["GET"])
 def get_products ():
-     products = mongo.db.products
+     products = mongo.products
 
      output = []
 
