a1626ebcc258a85c85de0ab33fce7aa7374aeac8
khaled03282016 <omrikhaledr@gmail.com>
2020-10-22 12:28:10 -0400

    add port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, url_for, session, escape
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
@@ -28,4 +29,5 @@ def get_products ():
 
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0/0', debug=True)
+     port=port=int(os.environ.get('PORT',5000))
+     app.run(host='0.0.0.0', port=port, debug=True)
